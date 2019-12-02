e77463785b33e4865aded55194d42e91f2d8f7e1
WWWCourses <ivawebcourses@gmail.com>
2019-12-02 14:19:00 +0200

    Lab2: 02.12.2019 DONE: comments NEXT: next TODO: check TODO in files

---
+++ lab2/dataTypes.js
@@ -0,0 +1,67 @@
+// why is important to know data types?
+console.log( 2 + 3);
+console.log( 2 + '3');
+
+// numbers
+let x = -2.144;
+
+// 10 на 8ма степен
+// Todo: how exponent notation actually work
+console.log(10e8);
+console.log(10**8);
+
+
+// strings
+console.log(" pub is: \"Tom's pub\"");
+console.log(" pub is: \\\"Tom's pub\\\"");
+
+console.log('\a');
+console.log('>>>>\t<<<<');
+
+let poetry = "line 1\nline 2\nline 3"
+console.log(poetry);
+console.log(">\\n<");
+console.log(">\\\n<");
+
+// template string demo
+let userFirstName = 'Ada';
+let userLastName = 'Byron';
+
+let userName = userFirstName + ' ' + userLastName + "!";
+console.log(`${userFirstName} ${userLastName}!`);
+
+
+// example of objects - non atomaric data types
+let user = {
+    'name' : "Ada",
+    'age': 23
+}
+let newUser = ['Byron', 34]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('x = '+ x + ", y = " + y);
+console.log(`x = ${x}, y = ${y}`);
+
+let bool = true;
+
+let c = null;
+console.log(`Put comment: CTRL+/`);
+console.log( typeof(c) );
+
---
+++ lab2/syntaxIntro.js
@@ -0,0 +1,30 @@
+var x = 5;
+let y = 3;
+y = 10;
+
+const pi = 3.14;
+// pi = 10;
+console.log(pi);
+
+
+let z;
+console.log(z);
+
+z = x**2 + y**2;
+console.log(z);
+
+// variable names rules
+let $ = 30;
+console.log($);
+
+let _ = '------'
+console.log(_);
+
+
+// variables convention
+var userfirstname = "Ada"
+var user_last_name = "Ada"
+
+var userLastName = "Ada"
+
+console.log(user_last_name);
