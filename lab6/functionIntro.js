364bbc437b5f24eda0e1880ab10ab2fdb9f48d6b
WWWCourses <ivawebcourses@gmail.com>
2020-01-13 14:11:38 +0200

    Lab6: 13.01.2020 DONE: till https://wwwcourses.github.io/JS-Atendia-Slides/pages/themes/JS_Functions/JS_Functions.html#/10 NEXT: next

---
+++ lab6/functionIntro.js
@@ -0,0 +1,38 @@
+// let x = 4;
+
+
+// function sayHello(userName) {
+//     // let userName
+//     console.log(`~~~~~~~~~~~~~~~~~~~~~~~`);
+//     console.log(`Hello ${userName}!`);
+//     console.log(`~~~~~~~~~~~~~~~~~~~~~~~`);
+// }
+// sayHello("Ada"); // userName="Ada"
+// sayHello("Pesho"); // userName="Pesho"
+
+
+// task: write function which logs :
+// (x**2 + y**2)
+// function sum(a,b) {
+//     console.log(`1.`);
+//     return a+b;
+// }
+
+// console.log(`3.`);
+
+// console.log( "4." + sum(2,3) );
+
+
+
+function giveMeAnswer(x){
+    console.log(x);
+    return 42;
+}
+
+// giveMeAnswer() ще се замести с 42:
+
+let res = giveMeAnswer(9);
+console.log(res);
+
+//
+
---
+++ lab6/task.js
@@ -0,0 +1,24 @@
+// function sumEvenArrayElements(a) {
+//     // let a = arr1
+//     let sum = 0;
+
+//     for(let i = 0; i < a.length; i++) {
+//         sum += a[i];
+//     }
+
+//     console.log(`sum = ${sum}`);
+
+// }
+
+// let arr1 = [1,2,3,4,5];
+
+// console.log(`Elements of arr1:`);
+// sumEvenArrayElements(arr1);
+
+// console.log(`Elements of arr2:`);
+// sumEvenArrayElements([4,5]);
+
+// TASK: define function which logs only the negative array elements
+logNegativeElements([0,1,2,-8,-3]);
+// -8
+// -3
